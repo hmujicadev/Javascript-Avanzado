602db079bc283b3137ebcfeab96fcea825f02b19
hmujica <hmujicadev@gmail.com>
2019-07-06 17:15:06 -0300

    ejercicio 2

---
+++ ejercicio 2/app.js
@@ -0,0 +1,50 @@
+
+/* EJERCICIO 1
+
+let input2 = document.querySelector("#e2");
+let input1 = document.querySelector("#e1");
+
+input2.onblur = function(){console.log('hola');}
+
+input2.addEventListener('click',()=>{ 
+    console.log('hector');
+    if(input1.value  == input2.value){
+    }
+
+}); 
+
+function comparar(){
+    if(input1.value == input2.value){
+        input2.classList.add('ok');
+        input2.classList.remove('error');
+    }else{
+        input2.classList.remove('ok');
+        input2.classList.add('error');
+    }
+}
+
+input2.addEventListener('blur', comparar);
+
+function teSaludodenuevo(){
+    console.log('hola2');
+}
+function saludar(){
+    teSaludodenuevo();
+    console.log('hola1');
+
+}
+
+saludar(); */
+
+//EJERCICIO 2
+
+let habitaciones = document.querySelectorAll('.habitacion');
+
+
+
+function esconder(){
+    for (let i = 0; i < habitaciones.length; i++) {
+        habitaciones[i].classList.add('esconder');
+        
+    }
+}
